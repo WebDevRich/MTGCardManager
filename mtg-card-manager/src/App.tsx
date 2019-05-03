a7bd0d215befad202cf201cd6e8b1b2a901ea06e
RichMorris <richard.morris@lovehoney.co.uk>
2019-05-03 11:22:58 +0100

    Finishing setup

--- mtg-card-manager/src/App.tsx
+++ mtg-card-manager/src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			{/* Material-UI CSS resets */}
+			<CssBaseline />
+
+			{/* App */}
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>
+						Edit <code>src/App.tsx</code> and save to reload.
+					</p>
+					<Button variant="contained" color="primary">
+						Hello World
+					</Button>
+				</header>
+			</div>
+		</>
+	);
 }
 
 export default App;
