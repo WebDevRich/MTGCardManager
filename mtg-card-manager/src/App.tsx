d0b38601c4556615275f633409ee0c32ea210a12
RichMorris <richard.morris@lovehoney.co.uk>
2019-05-04 08:54:19 +0100

    Fetch cards and put them in to an array, save to state

--- mtg-card-manager/src/App.tsx
+++ mtg-card-manager/src/App.tsx
@@ -1,29 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.scss';
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-const App: React.FC = () => {
-	return (
-		<>
-			{/* Material-UI CSS resets */}
-			<CssBaseline />
+interface AppState {
+	cards: [];
+}
 
-			{/* App */}
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.tsx</code> and save to reload.
-					</p>
-					<Button variant="contained" color="primary">
-						Hello World
-					</Button>
-				</header>
-			</div>
-		</>
-	);
+export class App extends React.PureComponent<{}, AppState> {
+
+	constructor(props:any) {
+		super(props);
+
+		this.state = {
+			cards: [],
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://api.scryfall.com/cards/search?order=released&q=riot')
+			.then(response => response.json())
+			.then(response => {
+				let cards = response.data.map((cardPic:any) => {
+					return(
+						<div className='search-result' key={cardPic.results}>
+							<img src={cardPic.image_uris.normal} alt='' />
+						</div>
+					)
+				})
+				this.setState({
+					cards: cards,
+				});
+			})
+			.catch(error =>
+				console.log(error)
+			)
+	}
+
+	public render() {
+
+		return (
+			<>
+				{/* Material-UI CSS resets */}
+				<CssBaseline />
+
+				{/* App */}
+				<div className='App'>
+					{this.state.cards}
+				</div>
+			</>
+		);
+	}
 }
 
 export default App;
