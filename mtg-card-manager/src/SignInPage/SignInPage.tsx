e564b5d3db9dc6899dbf6092564ec3f4b540a038
RichMorris <richard.morris@lovehoney.co.uk>
2019-05-20 09:04:40 +0100

    un-required imports

--- mtg-card-manager/src/SignInPage/SignInPage.tsx
+++ mtg-card-manager/src/SignInPage/SignInPage.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
