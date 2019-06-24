6923d083f4d587ba94c9c3b69b42f99862db184d
RichMorris <richard.morris@lovehoney.co.uk>
2019-06-24 16:39:04 +0100

    Trying to put the navbar and menu on the same page just to get it working (it doesn't)

--- mtg-card-manager/src/MainPage/MainPage.tsx
+++ mtg-card-manager/src/MainPage/MainPage.tsx
@@ -1,12 +1,17 @@
-import { Divider, Drawer, IconButton, List, ListItem, ListItemText } from '@material-ui/core';
+import { AppBar, Divider, Drawer, IconButton, List, ListItem, ListItemText, Toolbar } from '@material-ui/core';
 import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import clsx from 'clsx';
 import * as React from 'react';
+import ButtonComponent from '../ButtonComponent/ButtonComponent';
 import CardGrid from '../CardGrid/CardGrid';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
-import SearchBar from '../SearchBar/SearchBar';
 import SingleCard from '../SingleCard/SingleCard';
+import TextInput from '../TextInput/TextInput';
 import { theme } from '../theme';
 import TransformCard from '../TransformCard/TransformCard';
 
@@ -14,20 +19,66 @@ const drawerWidth = 240;
 
 const styles = (uiTheme:Theme) =>
 	createStyles({
-	drawer: {
-		width: drawerWidth,
-		flexShrink: 0,
-	},
-	drawerPaper: {
-		width: drawerWidth,
-	},
-	drawerHeader: {
-		display: 'flex',
-		alignItems: 'center',
-		padding: '0 8px',
-		...uiTheme.mixins.toolbar,
-		justifyContent: 'flex-end',
-	},
+		drawer: {
+			width: drawerWidth,
+			flexShrink: 0,
+		},
+		drawerPaper: {
+			width: drawerWidth,
+		},
+		drawerHeader: {
+			display: 'flex',
+			alignItems: 'center',
+			padding: '0 8px',
+			...uiTheme.mixins.toolbar,
+			justifyContent: 'flex-end',
+		},
+		searchBar: {
+			gridColumn: '1 / -1',
+			padding: 20,
+			display: 'flex',
+			justifyContent: 'center',
+			backgroundColor: theme.palette.secondary.dark,
+			color: theme.palette.secondary.contrastText,
+		},
+		search: {
+			position: 'relative',
+			borderRadius: theme.shape.borderRadius,
+			backgroundColor: fade(theme.palette.common.white, 0.15),
+			'&:hover': {
+			backgroundColor: fade(theme.palette.common.white, 0.25),
+			},
+				marginLeft: 0,
+				marginRight: theme.spacing.unit,
+			width: '100%',
+			[theme.breakpoints.up('sm')]: {
+			marginLeft: theme.spacing.unit,
+			width: 'auto',
+			},
+			},
+			searchForm: {
+				display: 'flex;',
+			},
+			appBar: {
+			transition: theme.transitions.create(['margin', 'width'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+			}),
+		},
+		appBarShift: {
+			width: `calc(100% - ${drawerWidth}px)`,
+			marginLeft: drawerWidth,
+			transition: theme.transitions.create(['margin', 'width'], {
+				easing: theme.transitions.easing.easeOut,
+				duration: theme.transitions.duration.enteringScreen,
+			}),
+		},
+		menuButton: {
+		marginRight: theme.spacing.unit * 2,
+		},
+		hide: {
+			display: 'none',
+		},
 });
 
 interface MainPageState {
@@ -59,6 +110,7 @@ export class MainPage extends React.PureComponent<MainPageProps, MainPageState> 
 		this.hasErrored = this.hasErrored.bind(this);
 		this.submitSearch = this.submitSearch.bind(this);
 		this.handleDrawerClose = this.handleDrawerClose.bind(this);
+		this.handleDrawerOpen = this.handleDrawerOpen.bind(this);
 	}
 
 	// componentWillMount() {
@@ -85,8 +137,9 @@ export class MainPage extends React.PureComponent<MainPageProps, MainPageState> 
 		});
 	}
 
-	private submitSearch(searchTerm:string) {
-		fetch(`https://api.scryfall.com/cards/search?order=released&q=${searchTerm}`)
+	private submitSearch(e:React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>) {
+		e.preventDefault();
+		fetch(`https://api.scryfall.com/cards/search?order=released&q=${this.state.searchTerm}`)
 			.then(response => response.json())
 			.then(response => {
 				const cards = response.data.map((cardItem:any, index:number) => {
@@ -116,9 +169,25 @@ export class MainPage extends React.PureComponent<MainPageProps, MainPageState> 
 			});
 	}
 
+	private searchTerm(value:any) {
+		this.setState({
+			searchTerm: value,
+		});
+	}
+
 	private handleDrawerClose() {
 		this.setState({
 			setOpen: false,
+		}, () => {
+			console.log('close');
+		});
+	}
+
+	private handleDrawerOpen() {
+		this.setState({
+			setOpen: true,
+		}, () => {
+			console.log('open');
 		});
 	}
 
@@ -128,11 +197,38 @@ export class MainPage extends React.PureComponent<MainPageProps, MainPageState> 
 
 		return (
 			<>
-				<SearchBar
-					// searchSuggestions={this.state.cardNames}
-					submitSearch={this.submitSearch}
-					hasErrored={this.hasErrored}
-				/>
+				<div className={classes.searchBar}>
+					<AppBar
+						position='fixed'
+						className={clsx(classes.appBar, {
+							[classes.appBarShift]: this.state.open,
+						})}
+					>
+						<Toolbar>
+							<IconButton
+								color='inherit'
+								aria-label='Open drawer'
+								onClick={this.handleDrawerOpen}
+								// edge="start"
+								className={clsx(classes.menuButton, this.state.open && classes.hide)}
+							>
+								<MenuIcon />
+							</IconButton>
+						</Toolbar>
+						<form noValidate={true} className={classes.searchForm} onSubmit={this.submitSearch}>
+							<div className={classes.search}>
+								<TextInput
+									inputValue={this.searchTerm}
+									placeholder='Search...'
+									// searchSuggestions={this.props.searchSuggestions}
+								/>
+							</div>
+							<ButtonComponent onClick={this.submitSearch} color='primary' type='submit' variant='contained'>
+								<SearchIcon />
+							</ButtonComponent>
+						</form>
+					</AppBar>
+				</div>
 				<Drawer
 					className={classes.drawer}
 					variant='persistent'
