e2b94056347cf1acaddd8f13197bbc9b4c9d7fcd
RichMorris <richard.morris@lovehoney.co.uk>
2019-12-24 09:44:33 +0000

    Refactored to function based component to make use of hooks

--- mtg-card-manager/src/SignInForm/SignInForm.tsx
+++ mtg-card-manager/src/SignInForm/SignInForm.tsx
@@ -4,16 +4,16 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import Paper from '@material-ui/core/Paper';
-import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import * as EmailValidator from 'email-validator';
-import * as React from 'react';
+import React, { useState } from 'react';
 import { loginUser, registerUser } from '../actions/authActions';
 import ButtonComponent from '../ButtonComponent/ButtonComponent';
 import PasswordField from '../PasswordField/PasswordField';
 
-const styles = (theme:Theme) =>
+const useStyles = makeStyles((theme:Theme) =>
 	createStyles({
 		main: {
 			width: 'auto',
@@ -41,221 +41,156 @@ const styles = (theme:Theme) =>
 			width: '100%', // Fix IE 11 issue.
 			marginTop: theme.spacing(1),
 		},
-	});
+	}));
 
-export interface SignInFormProps extends WithStyles<typeof styles> {
+export interface SignInFormProps {
 	signedIn(success:boolean):void;
 }
 
-export interface SignInFormState {
-	emailError: boolean;
-	emailValue: string;
-	existingUser: boolean;
-	passwordError: boolean;
-	passwordValue: string;
-	submitError: boolean;
-	signedIn: boolean;
-}
+export default function SignInForm(props:SignInFormProps) {
 
-export class SignInForm extends React.PureComponent<SignInFormProps, SignInFormState> {
+	const classes = useStyles(props);
+	const [emailValue, setEmailValue] = useState('');
+	const [passwordValue, setPasswordValue] = useState('');
+	const [passwordError, setPasswordError] = useState(false);
+	const [emailError, setEmailError] = useState(false);
+	const [submitError, setSubmitError] = useState(false);
+	const [existingUser, setExistingUser] = useState(false);
 
-	constructor(props:SignInFormProps) {
-		super(props);
-
-		this.state = {
-			emailError: false,
-			emailValue: '',
-			existingUser: false,
-			passwordError: false,
-			passwordValue: '',
-			submitError: false,
-			signedIn: false,
-		};
-
-		this.passwordValueUpdate = this.passwordValueUpdate.bind(this);
-		this.formSubmit = this.formSubmit.bind(this);
-		this.formSubmitButton = this.formSubmitButton.bind(this);
-		this.toggleExistingUser = this.toggleExistingUser.bind(this);
-		this.updateEmailValue = this.updateEmailValue.bind(this);
-		this.handleErrorResponse = this.handleErrorResponse.bind(this);
+	function updateEmailValue(e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+		setEmailValue(e.currentTarget.value);
 	}
 
-	private errorMessage = '';
-
-	private updateEmailValue(e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
-		const newEmailValue = e.currentTarget.value;
-		this.setState({
-			emailValue: newEmailValue,
-		});
-	}
-
-	private passwordValueUpdate(newPasswordValue:string) {
-		this.setState({
-			passwordValue: newPasswordValue,
-		});
+	function passwordValueUpdate(newPasswordValue:string) {
+		setPasswordValue(newPasswordValue);
 	}
 
 	// Validate email
-	private validateEmail() {
+	function validateEmail() {
 		let isValid = false;
-		if (!EmailValidator.validate(this.state.emailValue)) {
+		if (!EmailValidator.validate(emailValue)) {
 			isValid = false;
-			this.setState({
-				emailError: true,
-			});
+			setEmailError(true);
 		} else {
 			isValid = true;
-			this.setState({
-				emailError: false,
-			});
+			setEmailError(false);
 		}
 
 		return isValid;
 	}
 
 	// Validate password
-	private validatePassword() {
+	function validatePassword() {
 		let isValid = false;
-		if (this.state.passwordValue.length < 6) {
+		if (passwordValue.length < 6) {
 			isValid = false;
-			this.setState({
-				passwordError: true,
-			});
+			setPasswordError(true);
 		} else {
 			isValid = true;
-			this.setState({
-				passwordError: false,
-			});
+			setPasswordError(false);
 		}
 
 		return isValid;
 	}
 
-	private handleErrorResponse(response:any) {
+	function handleErrorResponse(response:any) {
 		console.log(response);
 		if (response.data.email === 'Email already exists') {
 			console.log(response.data.email);
-			this.setState({
-				submitError: true,
-				existingUser: true,
-			}, () => {
-				this.errorMessage = 'An account already exists with this email, please sign in.';
-			});
+			setSubmitError(true);
+			setExistingUser(true);
 		} else if (response.data.emailnotfound || response.data.passwordincorrect) {
-			this.setState({
-				submitError: true,
-			}, () => {
-				this.errorMessage = 'Either the password or email is incorrect';
-			});
+			setSubmitError(true);
 		} else {
-			this.setState({
-				submitError: true,
-			}, () => {
-				this.errorMessage = 'Sorry, somethging went wrong. Please try again';
-			});
+			setSubmitError(true);
 		}
 	}
 
-	private formSubmit() {
-		if (this.validateEmail() && this.validatePassword()) {
-			this.setState({
-				signedIn: true,
-			}, () => {
-				const userData = {
-					email: this.state.emailValue,
-					password: this.state.passwordValue,
-				};
-				if (!this.state.existingUser) {
-					registerUser(userData,
-						(response:any) => {
-							if (response.status !== 200) {
-								this.handleErrorResponse(response);
-							}
-						},
-					);
-
-				} else {
-					loginUser(userData,
-						(response:any) => {
-							if (response.status !== 200) {
-								this.handleErrorResponse(response);
-							}
-						},
-					);
-				}
-			});
-		}
-	}
-
-	private formSubmitButton(e:React.MouseEvent<HTMLButtonElement>) {
-		e.preventDefault();
-		this.formSubmit();
-	}
-
-	private toggleExistingUser() {
-		this.setState(prevState => ({
-			existingUser: !prevState.existingUser,
-		}));
-	}
-
-	public render() {
-
-		const { classes } = this.props;
-
-		return(
-			<main className={classes.main}>
-				<Paper className={classes.paper}>
-					<Avatar className={classes.avatar}>
-						<LockOutlinedIcon />
-					</Avatar>
-					<Typography component='h1' variant='h5'>
-						Sign in / Register
-					</Typography>
-					<form className={classes.form} onSubmit={this.formSubmit}>
-						<FormControl margin='normal' error={this.state.emailError} required={true} fullWidth={true}>
-							<InputLabel	htmlFor='email'>Email Address</InputLabel>
-							<Input
-								id='email'
-								name='email'
-								autoComplete='email'
-								autoFocus={true}
-								onChange={this.updateEmailValue}
-							/>
-							{this.state.emailError &&
-								<FormHelperText>Please enter a valid email</FormHelperText>
-							}
-						</FormControl>
-						<FormControl margin='normal' error={this.state.passwordError} required={true} fullWidth={true}>
-							<InputLabel htmlFor='password'>Password</InputLabel>
-							<PasswordField passwordError={this.state.passwordError} onChange={this.passwordValueUpdate} />
-						</FormControl>
-						<ButtonComponent
-							onClick={this.formSubmitButton}
-							fullWidth={true}
-							color='primary'
-							type='submit'
-							variant='contained'
-						>
-							{this.state.existingUser ? 'Sign In' : 'Register'}
-						</ButtonComponent>
-						{this.state.submitError &&
-							<FormHelperText error={true}>An account already exists with this email, please sign in.</FormHelperText>
+	function formSubmit() {
+		if (validateEmail() && validatePassword()) {
+			const userData = {
+				email: emailValue,
+				password: passwordValue,
+			};
+			if (!existingUser) {
+				registerUser(userData,
+					(response:any) => {
+						if (response.status !== 200) {
+							handleErrorResponse(response);
 						}
-					</form>
-					<ButtonComponent
-						onClick={this.toggleExistingUser}
-						fullWidth={true}
-						color='default'
-						variant='text'
-						type='submit'
-					>
-						{!this.state.existingUser ? 'Already have an account? Sign In...' : 'New here? Register...'}
-					</ButtonComponent>
-				</Paper>
-			</main>
-		);
-
+					},
+				);
+			} else {
+				loginUser(userData,
+					(response:any) => {
+						if (response.status !== 200) {
+							handleErrorResponse(response);
+						}
+					},
+				);
+			}
+		}
 	}
-}
 
-export default withStyles(styles)(SignInForm);
+	function formSubmitButton(e:React.MouseEvent<HTMLButtonElement>) {
+		e.preventDefault();
+		formSubmit();
+	}
+
+	function toggleExistingUser() {
+		setExistingUser(!existingUser);
+	}
+
+	return(
+		<main className={classes.main}>
+			<Paper className={classes.paper}>
+				<Avatar className={classes.avatar}>
+					<LockOutlinedIcon />
+				</Avatar>
+				<Typography component='h1' variant='h5'>
+					Sign in / Register
+				</Typography>
+				<form className={classes.form} onSubmit={formSubmit}>
+					<FormControl margin='normal' error={emailError} required={true} fullWidth={true}>
+						<InputLabel	htmlFor='email'>Email Address</InputLabel>
+						<Input
+							id='email'
+							name='email'
+							autoComplete='email'
+							autoFocus={true}
+							onChange={updateEmailValue}
+						/>
+						{emailError &&
+							<FormHelperText>Please enter a valid email</FormHelperText>
+						}
+					</FormControl>
+					<FormControl margin='normal' error={passwordError} required={true} fullWidth={true}>
+						<InputLabel htmlFor='password'>Password</InputLabel>
+						<PasswordField passwordError={passwordError} onChange={passwordValueUpdate} />
+					</FormControl>
+					<ButtonComponent
+						onClick={formSubmitButton}
+						fullWidth={true}
+						color='primary'
+						type='submit'
+						variant='contained'
+					>
+						{existingUser ? 'Sign In' : 'Register'}
+					</ButtonComponent>
+					{submitError &&
+						<FormHelperText error={true}>An account already exists with this email, please sign in.</FormHelperText>
+					}
+				</form>
+				<ButtonComponent
+					onClick={toggleExistingUser}
+					fullWidth={true}
+					color='default'
+					variant='text'
+					type='submit'
+				>
+					{!existingUser ? 'Already have an account? Sign In...' : 'New here? Register...'}
+				</ButtonComponent>
+			</Paper>
+		</main>
+	);
+}
